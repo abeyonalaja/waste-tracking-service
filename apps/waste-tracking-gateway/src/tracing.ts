21c6feda9b5596cf815a7f08ca6662833eb00675
iwalters <iwalters@hitachisolutions.com>
2023-04-11 09:29:20 +0000

    Merged PR 28496: Azure Monitor tracing for waste-tracking-gateway
    Azure Monitor tracing for waste-tracking-gateway
    
    Added Application Insights tracing to _waste-tracking-gateway_ via [OpenTelemetry](https://opentelemetry.io/).
    
     - Downgraded Hapi from v21 to v20 because unfortunately instrumentation is [not yet provided for v21](https://github.com/open-telemetry/opentelemetry-js-contrib/issues/1383). If this becomes a problem, we can forgo the Hapi layer and rely on just HTTP instrumentation.
     - Traces will be written to console in development.
     - Setting `APPINSIGHTS_CONNECTION_STRING` emits traces to Azure Monitor.
     - This required an additional _tracing.js_ entry point as instrumentation must be loaded first.

---
+++ apps/waste-tracking-gateway/src/tracing.ts
@@ -0,0 +1,36 @@
+import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { HapiInstrumentation } from '@opentelemetry/instrumentation-hapi';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { Resource } from '@opentelemetry/resources';
+import {
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+} from '@opentelemetry/sdk-trace-base';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const provider = new NodeTracerProvider({
+  resource: Resource.default().merge(
+    new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]:
+        process.env['APP_ID'] || 'waste-tracking-gateway',
+    })
+  ),
+});
+
+if (process.env['NODE_ENV'] === 'development') {
+  provider.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+}
+
+const connectionString = process.env['APPINSIGHTS_CONNECTION_STRING'];
+if (connectionString !== undefined) {
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(new AzureMonitorTraceExporter({ connectionString }))
+  );
+}
+
+provider.register();
+registerInstrumentations({
+  instrumentations: [new HttpInstrumentation(), new HapiInstrumentation()],
+});
