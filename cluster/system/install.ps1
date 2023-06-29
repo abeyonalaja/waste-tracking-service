d99c9c7f8e2d4e80ef89a06b6a546c37080e7357
ddragieva <ddragieva@hitachisolutions.com>
2023-06-29 06:36:15 +0000

    Merged PR 33341: CI Pipeline App Deployments
    CI Pipeline App Deployments:
    
    - Templated common elements
    - Conditional execution of steps based on output from _Build_ job
    - Checks on Helm Chart version deployed
    
    Related work items: #157998

--- cluster/system/install.ps1
+++ cluster/system/install.ps1
@@ -7,15 +7,6 @@
 # kubectl and kubelogin: az aks install-cli
 # helm download: https://github.com/helm/helm/releases
 
-az login
-
-$WtsResourceGroupName = "SNDWTSAKSRG2401"
-$AksName = "SNDWTSSPKKS2401"
-az aks get-credentials -n $AksName -g $WtsResourceGroupName
-
-kubectl get nodes
-# It will prompt you to do the device code flow through the browser
-
 $NginxNamespace = "nginx-system"
 # NGINX Inc IC:
 # $NginxReleaseName = "nginx-ingress"
@@ -28,7 +19,7 @@ $NginxReleaseName = "ingress-nginx"
 $NginxHelmRepoName = "ingress-nginx"
 $NginxHelmRepoUrl = "https://kubernetes.github.io/ingress-nginx"
 $NginxHelmChartName = "ingress-nginx/ingress-nginx"
-$NginxValuesFileName = "./values-k8s-nginx.yaml"
+$NginxValuesFileName = "./values-nginx.yaml"
 
 helm repo add $NginxHelmRepoName $NginxHelmRepoUrl
 helm repo update
@@ -38,49 +29,3 @@ helm install $NginxReleaseName $NginxHelmChartName `
     -f $NginxValuesFileName 
 
 # Wait for ALB Internal/External IP address to propagate onto the NGINX Ingress Controller svc loadBalancer <EXTERNAL-IP> field
-
-# This will ask Is Dapr already installed in the cluster? (y/N): N
-az k8s-extension create --cluster-type managedClusters `
-    --cluster-name $AksName `
-    --resource-group $WtsResourceGroupName `
-    --name dapr `
-    --extension-type Microsoft.Dapr `
-    --auto-upgrade-minor-version true `
-    --configuration-settings "global.ha.enabled=true" `
-    --configuration-settings "dapr_operator.replicaCount=2"
-
-# To open the Dapr dashboard
-dapr dashboard -k
-
-# If cluster OIDC issuer is not enable since AKS creation
-az aks update -g $WtsResourceGroupName -n $AksName --enable-oidc-issuer
-
-# EnableWorkloadIdentityPreview feature has to be registered to Microsoft.ContainerService
-az feature register --namespace "Microsoft.ContainerService" --name "EnableWorkloadIdentityPreview"
-
-# Bicep template deployment does not require anything further
-# --enable-workload-identity comes from az CLI aks-preview as of 29/03/2023
-# az extension add --name aks-preview
-# az aks update -g $WtsResourceGroupName -n $AksName --enable-workload-identity
-# Use Helm chart if any issue with public preview (Helm chart is GA)
-# https://azure.github.io/azure-workload-identity/docs/installation/mutating-admission-webhook.html
-
-# For auto-updates, cluster can be upgraded, see property:
-# autoUpgradeProfile: {
-#     nodeOSUpgradeChannel: 'NodeImage'
-#     upgradeChannel: 'stable'
-#   }
-$KuredNamespace = "kured-system"
-$KuredReleaseName = "kured"
-$KuredHelmRepoName = "kubereboot"
-$KuredHelmRepoUrl = "https://kubereboot.github.io/charts"
-$KuredHelmChartName = "kubereboot/kured"
-$KuredValuesFileName = "./values-kured.yaml"
-
-helm repo add $KuredHelmRepoName $KuredHelmRepoUrl
-helm repo update
-helm install $KuredReleaseName $KuredHelmChartName `
-    --create-namespace `
-    -n $KuredNamespace `
-    -f $KuredValuesFileName 
-
