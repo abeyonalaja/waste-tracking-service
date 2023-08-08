7623b7c768ebdf54f2b5934db2355586397d5ed9
James King <jking@hitachisolutions.com>
2023-08-08 13:33:58 +0000

    Merged PR 37136: Fix depreciation warning
    Fix depreciation warning

--- apps/waste-tracking-service/pages/api/pdf.tsx
+++ apps/waste-tracking-service/pages/api/pdf.tsx
@@ -1,7 +1,10 @@
 import puppeteer from 'puppeteer';
 
 async function createPDF(hostname, id) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto(`http://${hostname}/download-report?id=${id}`, {
     waitUntil: 'networkidle0',
