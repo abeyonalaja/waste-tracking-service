a03f165ae1724cb9c511b568f2145d771317fb18
James King <jking@hitachisolutions.com>
2024-01-09 16:37:29 +0000

    Merged PR 47000: Replace encoded question mark in URL

--- apps/waste-tracking-service/middleware.ts
+++ apps/waste-tracking-service/middleware.ts
@@ -1,12 +1,23 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      return !!token;
-    },
+export default withAuth(
+  function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname.includes('%3F')) {
+      return NextResponse.redirect(
+        new URL(req.nextUrl.pathname.replace('%3F', '?'), req.url)
+      );
+    }
   },
-});
+  {
+    callbacks: {
+      authorized({ req, token }) {
+        return !!token;
+      },
+    },
+  }
+);
 
 export const config = {
   matcher: ['/export/:path*'],
