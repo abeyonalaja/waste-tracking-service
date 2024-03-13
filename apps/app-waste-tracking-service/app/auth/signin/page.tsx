05425ba1fc95bab53ceb78779031fa65b54f5f27
James King <jking@hitachisolutions.com>
2024-03-13 17:37:20 +0000

    Merged PR 52304: Fix the build

--- apps/app-waste-tracking-service/app/auth/signin/page.tsx
+++ apps/app-waste-tracking-service/app/auth/signin/page.tsx
@@ -4,32 +4,48 @@ import { useTranslation } from '../../../utils/useTranslation';
 import { signIn, useSession } from 'next-auth/react';
 import React, { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function Index() {
+const Loading = () => {
+  return <>Loading</>;
+};
+
+const PageContent = () => {
   const { t } = useTranslation('auth');
-  const router = useRouter();
   const searchParams = useSearchParams();
+  const router = useRouter();
   const { data: session } = useSession();
+
   useEffect(() => {
     if (session) {
       const callbackUrl = searchParams.get('callbackUrl');
       router.push(callbackUrl !== null ? callbackUrl.toString() : '/account');
     }
-  }, [session, searchParams]);
+  }, [session]);
+  return (
+    <>
+      <GovUK.Heading size={'l'} level={1}>
+        {t('signinPage.title')}
+      </GovUK.Heading>
+      <GovUK.Button
+        onClick={() => {
+          signIn('defra-b2c');
+        }}
+      >
+        {t('signinPage.button')}
+      </GovUK.Button>
+    </>
+  );
+};
+
+export default function Index() {
   return (
     <>
       <GovUK.GridRow>
         <GovUK.GridCol size={'two-thirds'}>
-          <GovUK.Heading size={'l'} level={1}>
-            {t('signinPage.title')}
-          </GovUK.Heading>
-          <GovUK.Button
-            onClick={() => {
-              signIn('defra-b2c');
-            }}
-          >
-            {t('signinPage.button')}
-          </GovUK.Button>
+          <Suspense fallback={<Loading />}>
+            <PageContent />
+          </Suspense>
         </GovUK.GridCol>
       </GovUK.GridRow>
     </>
