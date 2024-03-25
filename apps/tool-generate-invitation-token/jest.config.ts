f6c9dc882f000aaf77b0b446a11dc98325e49099
ddragieva <ddragieva@hitachisolutions.com>
2024-03-25 11:31:46 +0000

    Merged PR 53173: Rename BE tool projects
    Rename BE tool projects
    
    Related work items: #346451

--- apps/tool-generate-invitation-token/jest.config.ts
+++ apps/tool-generate-invitation-token/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'generate-invitation-token',
+  displayName: 'tool-generate-invitation-token',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/generate-invitation-token',
+  coverageDirectory: '../../coverage/apps/tool-generate-invitation-token',
 };
