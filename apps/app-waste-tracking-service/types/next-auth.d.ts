6c040add8f41ce2390a9445e7fd3a3f15a2094a7
James King <jking@hitachisolutions.com>
2024-04-18 16:09:30 +0000

    Merged PR 54997: Reconfigure DCID on UKWM and fix redirect issue
    Related work items: #347429

---
+++ apps/app-waste-tracking-service/types/next-auth.d.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session {
+    token: string | null | undefined;
+  }
+  interface Profile {
+    sup: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    uniqueReference: string;
+  }
+  interface Account {
+    id_token_expires_in: number;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id_token: string | null | undefined;
+    refreshToken: string | null | undefined;
+    idTokenExpires: number;
+  }
+}
