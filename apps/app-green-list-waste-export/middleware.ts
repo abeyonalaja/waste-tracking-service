5786662cf748911937167870f7a1eba22fe01962
James King <jking@hitachisolutions.com>
2024-06-21 09:57:30 +0000

    Merged PR 59188: GLW redirect issue after coming from WTS project

--- apps/app-green-list-waste-export/middleware.ts
+++ apps/app-green-list-waste-export/middleware.ts
@@ -1,11 +1,28 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest): NextResponse<unknown> {
-  if (req.nextUrl.pathname.includes('%3F')) {
-    return NextResponse.redirect(
-      new URL(req.nextUrl.pathname.replace('%3F', '?'), req.url),
-    );
+export function middleware(req: NextRequest): NextResponse {
+  const textUrl: string = req.nextUrl.pathname;
+  let newUrl: string = textUrl.toString();
+  let redirectRequired = false;
+
+  if (newUrl.includes('%3F')) {
+    newUrl = newUrl.replace('%3F', '?');
+    redirectRequired = true;
+  }
+
+  if (newUrl.includes('/en/')) {
+    newUrl = newUrl.replace('/en/', '/');
+    redirectRequired = true;
+  }
+
+  if (newUrl.includes('/cy/')) {
+    newUrl = newUrl.replace('/cy/', '/');
+    redirectRequired = true;
+  }
+
+  if (redirectRequired) {
+    return NextResponse.redirect(new URL(newUrl, req.url));
   }
 }
 
