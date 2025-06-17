11ed52cae136a8fa22f06d591bd54e2ac11c0e04
Adam <adam@lumesoftware.com>
2025-06-17 15:03:41 +0100

    DWT-225 Add Property Descriptions table and dynamic parsing logic to API Specifications page

--- docs/apiSpecifications/dist/swagger-initializer.js
+++ docs/apiSpecifications/dist/swagger-initializer.js
@@ -1,3 +1,19 @@
+function onSwaggerRender() {
+  setTimeout(function() {
+    const properties = getProperties();
+
+    const propertyTable = document.getElementById('propertyDescriptions');
+    if (propertyTable) {
+      for(const property in properties) {
+        const newRow = propertyTable.insertRow();
+        newRow.insertCell().innerText = property;
+        newRow.insertCell().innerText = properties[property].isMandatory ? "Mandatory" : "Optional";
+        newRow.insertCell().innerHTML = properties[property].description;
+      }
+    }
+  }, 100)
+}
+
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
@@ -13,8 +29,33 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    onComplete: onSwaggerRender,
+    defaultModelsExpandDepth: 1000,
+    defaultModelExpandDepth: 1000,
+    defaultModelRendering: "model",
+    docExpansion: "full"
   });
 
   //</editor-fold>
 };
+
+function getProperties() {
+  const properties = {};
+  document.querySelectorAll('.property-row').forEach((element) => {
+    const description = element.querySelector('.renderedMarkdown');
+    if (description) {
+      let propertyName = element.firstChild.innerText;
+      let isMandatory = false;
+      if (propertyName.slice(-1) === '*') {
+        isMandatory = true;
+      }
+      propertyName = propertyName.split('*')[0];
+      properties[propertyName] = {
+        description: description.innerHTML,
+        isMandatory: isMandatory
+      };
+    }
+  });
+  return properties;
+}
